5a0ba93f60444da3edd2be0a1e44f095dabb18ec
Sunny Raj Rathod <sunnyrajrathod@users.noreply.github.com>
2016-08-12 19:49:20 +0530

    Update api_transaction.rb

--- lib/authorize_net/api/api_transaction.rb
+++ lib/authorize_net/api/api_transaction.rb
@@ -56,9 +56,9 @@ module AuthorizeNet::API
      unless responseClass.nil? or request.nil?
        begin
         @xml = serialize(request,type)
-        print @xml
+        puts @xml
         respXml = send_request(@xml)
-        print respXml.body
+        puts respXml.body
         @response = deserialize(respXml.body,responseClass) 
        rescue Exception => ex  
           ex  
