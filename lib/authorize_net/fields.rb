AuthorizeNet/sdk-ruby

commit 69ee4fc6d3352f38531d5c7accbc3afb9c84f03a

Add solution id to the AIM transaction objects
Solution id provides the functionality to review transactions on the basis
of single id for a partner devloper. So adding an option to provide the
solution_id when the transaction is created in old model for AIM
transactions. Also adding a test case to check the solution_id is set
properly from the response returned from the server.

Signed-off-by: Srijan Misra <srmisra@WW975L29177B.visa.com>

lib/authorize_net/fields.rb
@@ -49,6 +49,7 @@ module AuthorizeNet
         :md5_hash,
         :card_code_response,
         :cardholder_authentication_verification_response,
+        :solution_id,
         nil,
         nil,
         nil,

lib/authorize_net/aim/transaction.rb
@@ -71,6 +71,7 @@ module AuthorizeNet::AIM
       @verify_ssl = options[:verify_ssl]
       @market_type = options[:market_type]
       @device_type = options[:device_type]
+      @solution_id = options[:solution_id]
     end
     
     # Checks if the transaction has been configured for test mode or not. Return TRUE if the
@@ -127,6 +128,10 @@ module AuthorizeNet::AIM
     def cp_version
       @cp_version
     end
+
+    def solution_id
+      @solution_id
+    end
     
     #:enddoc:
     protected
@@ -147,6 +152,7 @@ module AuthorizeNet::AIM
       fields[:test_request] = boolean_to_value(@test_mode)
       fields[:allow_partial_auth] = 'TRUE' if @allow_split_transaction
       fields[:encap_char] = @encapsulation_character unless @encapsulation_character.nil?
+      fields[:solution_id] = @solution_id unless @solution_id.nil?
       fields.each do |k, v|
         if @@boolean_fields.include?(k)
           fields[k] = boolean_to_value(v)

spec/aim_spec.rb
@@ -8,6 +8,7 @@ describe AuthorizeNet::AIM::Transaction do
       @api_key = creds['api_transaction_key']
       @api_login = creds['api_login_id']
       @md5_value = creds['md5_value']
+      @solution_id = "AAA100302"
     rescue Errno::ENOENT => e
       @api_key = "TEST"
       @api_login = "TEST"
@@ -183,6 +184,14 @@ describe AuthorizeNet::AIM::Transaction do
     transaction.set_shipping_address(address)
     transaction.purchase(@amount, @credit_card).should be_kind_of(AuthorizeNet::AIM::Response)
     transaction.response.success?.should be_truthy
+  end
+
+  it "should support setting solution id" do
+    transaction = AuthorizeNet::AIM::Transaction.new(@api_login, @api_key, :transaction_type => @type, :gateway => @gateway, :test => @test_mode, :solution_id => @solution_id)
+    transaction.purchase(@amount, @credit_card).should be_kind_of(AuthorizeNet::AIM::Response)
+    transaction.response.success?.should be_truthy
+    fields = transaction.response.transaction
+    (defined?(fields.solution_id) and fields.solution_id == 'AAA100302').should be_truthy
   end
   
   it "should support adding an email receipt" do

commit ff8d65fb3eda387a1f1780abd109b872aba81308

Fix regression introduced by 69ee4fc: AIM Fields list should contain 68 elements, not 69. Was causing appended custom fields not to be parsed correctly.

lib/authorize_net/fields.rb
@@ -57,7 +57,6 @@ module AuthorizeNet
         nil,
         nil,
         nil,
-        nil,
         :account_number,
         :card_type,
         :split_tender_id,